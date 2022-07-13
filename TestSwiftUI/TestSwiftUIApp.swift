74667d69a88f0973b51f041d960ee518253bcf84
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-13 10:45:01 -0700

    Initial Commit

---
+++ TestSwiftUI/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  TestSwiftUI
+//
+//  Created by Mitya Kim on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
